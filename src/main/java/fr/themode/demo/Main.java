a1f9aaa87acbee03ee364e373fef7ee64db0277f
TheMode <themode@outlook.fr>
2021-09-01 11:41:32 +0200

    Initial commit

---
+++ src/main/java/fr/themode/demo/Main.java
@@ -0,0 +1,68 @@
+package fr.themode.demo;
+
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.PlayerChatEvent;
+import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.instance.*;
+import net.minestom.server.instance.batch.ChunkBatch;
+import net.minestom.server.instance.block.Block;
+import net.minestom.server.utils.MathUtils;
+import net.minestom.server.world.biomes.Biome;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MinecraftServer minecraftServer = MinecraftServer.init();
+
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+        instanceContainer.setChunkGenerator(new GeneratorDemo());
+
+        // Add an event callback to specify the spawning instance (and the spawn position)
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(PlayerLoginEvent.class, event -> {
+            final Player player = event.getPlayer();
+            event.setSpawningInstance(instanceContainer);
+            player.setRespawnPoint(new Pos(0, 42, 0));
+        });
+        globalEventHandler.addListener(PlayerChatEvent.class, event -> {
+            System.gc();
+        });
+
+        MinecraftServer.getUpdateManager().addTickMonitor(tickMonitor ->
+                System.out.println("time: " + MathUtils.round(tickMonitor.getTickTime(), 3)));
+
+        minecraftServer.start("0.0.0.0", 25565);
+    }
+
+    private static class GeneratorDemo implements ChunkGenerator {
+
+        @Override
+        public void generateChunkData(@NotNull ChunkBatch batch, int chunkX, int chunkZ) {
+            // Set chunk blocks
+            for (byte x = 0; x < Chunk.CHUNK_SIZE_X; x++)
+                for (byte z = 0; z < Chunk.CHUNK_SIZE_Z; z++) {
+                    for (byte y = 0; y < 40; y++) {
+                        batch.setBlock(x, y, z, Block.STONE);
+                    }
+                }
+        }
+
+        @Override
+        public void fillBiomes(@NotNull Biome[] biomes, int chunkX, int chunkZ) {
+            Arrays.fill(biomes, Biome.PLAINS);
+        }
+
+        @Override
+        public List<ChunkPopulator> getPopulators() {
+            return null;
+        }
+    }
+}
